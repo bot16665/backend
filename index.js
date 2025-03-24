e569532ce47555a1688bcad0472f142d5c50f05f
bot16665 <azharshaikh40198@gmail.com>
2025-03-24 16:37:32 +0530

    fix: remove deprecated MongoDB connection options

--- index.js
+++ index.js
@@ -25,10 +25,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '../dist')));
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('MongoDB connection error:', err));
 
